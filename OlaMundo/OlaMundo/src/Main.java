8bc4147a4449e3b789ac0340517be7bf67d184d2
Davi Augusto <davi.cianorte@gmail.com>
2023-12-19 00:23:25 -0300

    acrescentando OlaMundo

---
+++ OlaMundo/OlaMundo/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------");
+        //System.out.println(" ");
+        System.out.println("Hello and welcome!");
+        System.out.println("\nMeu primeiro programa em Java");
+        System.out.println("versao: 1");
+        System.out.println("----------------------------------------");
+    }
+}
